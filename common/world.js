0f7c283a7dad7df963bc6c4ae06c23779f949df0
Michael Yu <michael.yang2357@gmail.com>
2013-08-23 13:50:00 -0400

    Bug fix: return onPlayerSuccess generated function

--- common/world.js
+++ common/world.js
@@ -321,6 +321,7 @@ World.prototype.createObjects = function() {
         var onPlayerSuccess = function (player) {
             player.items[Item.TYPES.objective].push(new Item(objectiveID, Item.TYPES.objective, null));
         }
+        return onPlayerSuccess;
     }
 	// Create rugs
 	var numRugs = 20;//Math.floor(Math.random() * 5 + 2);
