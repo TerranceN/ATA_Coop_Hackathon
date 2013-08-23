7ae357d449a49c9dc19c1fc6e524e25957461cef
Michael Yu <michael.yang2357@gmail.com>
2013-08-23 16:05:39 -0400

    Players stop interacting with interactives when controlForce is exerted or when attacking

--- server/playerControlled.js
+++ server/playerControlled.js
@@ -6,11 +6,13 @@ Player.prototype.createListeners = function (socket, isServer) {
     var player = this;
     socket.on('controlForce', function (data) {
         player.controlForce = new Vector2(data.x, data.y);
+        player.stopInteracting();
     });
 
     socket.on('attack', function (data) {
         player.angle = data['angle'];
         player.attackFrame = true;
+        player.stopInteracting();
     });
     
     socket.on('action', function (data) {
